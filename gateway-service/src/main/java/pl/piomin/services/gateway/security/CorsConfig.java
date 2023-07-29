61c4bfdb72b41fd97458ea85936d0a77b9eba41e
skyglass <skyglass2001@gmail.com>
2023-07-30 01:04:52 +0200

    init commit

---
+++ gateway-service/src/main/java/pl/piomin/services/gateway/security/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.ivanfranchin.moviesapi.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class CorsConfig  {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(@Value("${erp-app.redirect-url}") String erpAppRedirectUrl,
+                                       @Value("${movie-app.redirect-url}") String movieAppRedirectUrl) {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of(erpAppRedirectUrl, movieAppRedirectUrl));
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
